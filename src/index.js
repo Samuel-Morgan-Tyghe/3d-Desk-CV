cd78fb8fe22a0a77271e5534997715a2827bb099
Samuel Morgan-Tyghe <sa>
2020-12-17 19:16:54 +0000

    mixed content

--- src/index.js
+++ src/index.js
@@ -372,14 +372,7 @@ function onMouseMove(event) {
   }
 }
 
-function getTemp() {
-  Axios.get(
-    "http://api.weatherapi.com/v1/current.json?key=d7df60db6422414b9d1153848200912&q=Edinburgh"
-  ).then((response) => {
-    console.log(response.data.current.feelslike_c);
-    return response;
-  });
-}
+
 
 // function onMouseOut(event) {
 //   event.preventDefault();
